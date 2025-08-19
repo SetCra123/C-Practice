0dcac9db065b9f13f88d7fa13bbcb1440f03f664
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2025-08-18 23:50:50 -0400

    I changed the employees to start as empty dataset, employees can be added manually by user, added while loop, if-then to code

--- Program.cs
+++ Program.cs
@@ -7,17 +7,22 @@ namespace CatWorx.BadgeMaker
   {
     static void Main(string[] args)
     {
-      List<string> employees = new List<string>() { "adam", "amy" };
-      employees.Add("barbara");
-      employees.Add("billy");
-      Console.WriteLine("Please enter a name: ");
-      // Get a name from the console and assign it to a variable
-      string input = Console.ReadLine() ?? "";
-      employees.Add(input);
-      for (int i = 0; i < employees.Count; i++) 
-      {
-        Console.WriteLine(employees[i]);
-      }
+      static void Main(string[] args)
+{
+  while(true){
+  List<string> 
+  employees = new List<string>();
+  Console.WriteLine("Please enter a name: (Leave empty to exit) ");
+  string input = Console.ReadLine() ?? "";
+  if( input = "" ){
+    break;
+  }
+  employees.Add(input);
+  for (int i = 0; i < employees.Count; i++) 
+  {
+    Console.WriteLine(employees[i]);
+  }
+}
     }
   }
 }
